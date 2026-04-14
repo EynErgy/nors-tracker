3b71ad7a6abf41fd0d5aaafdc4f983557e13efbf
Nicolas Rosa <nicolas.rosa@nrjcorporation.com>
2026-04-14 15:52:23 +0200

    Working one

---
+++ ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apps : [{
+    name   : "tracker",
+    script : "./server.js"
+    args: "80"
+  }]
+}
--- server.js
+++ server.js
@@ -2,12 +2,25 @@ var express = require('express')
   , tracker = require('pixel-tracker')
   , app = express()
   , cookieParser = require('cookie-parser')
+  , path = require('path')
+  , fs = require('fs')
+  , util = require('util')
+  , moment = require('moment')
+  , serveIndex = require('serve-index')
+
+
 
 app.use(cookieParser())
+app.use('/static', express.static(path.join(__dirname, '/static')))
+app.use('/logs', express.static(path.join(__dirname, '/logs')), serveIndex(path.join(__dirname, '/logs'), {'icons': true}))
+
 
 tracker
   .use(function (error, result) {
     console.log(JSON.stringify(result, null, 2))
+    const timestamp = moment().format('YYMMDD-HHmmss')
+    var log_file = fs.createWriteStream(__dirname + '/logs/' + timestamp + '-access.log', {flags : 'w'})
+    log_file.write(util.format(result) + '\n')
   })
 
 app.all('/pixel', tracker.middleware)
